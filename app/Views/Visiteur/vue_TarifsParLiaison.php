ac9eb9e83e708d511b630872c65d15370d405026
Brodie Clarkson <brodie.clarkson2@gmail.com>
2026-04-23 16:29:32 +0200

    mauvais uc4

--- app/Views/Visiteur/vue_TarifsParLiaison.php
+++ app/Views/Visiteur/vue_TarifsParLiaison.php
@@ -28,16 +28,58 @@ foreach ($lesTarifs as $unTarif)
     echo "</TR>";
 }
 echo "</table>";*/
-
-foreach($lesPeriodes as $unePeriode)
-{
-    echo "<TR>";
-    echo "<TD>".$unePeriode->datedebut."</TD><TD>"
-    .$unePeriode->datefin."</TD>";
-    echo "</TR>";
-    $i += 1;
-}
-echo "</table>";
-
-
 ?>
+
+
+<table class="table table-striped">
+    <tr colspan = 3>
+        <th rowspan=2 colspan=1>Catégorie</th>
+        <th rowspan=2 colspan=1>Type</th>
+        <th rowspan=1 colspan=4>
+            Période
+        </th>
+    </tr>
+    <tr>
+        <?php
+            foreach ($periode as $uneLigne) :
+                echo '<th rowspan=1>'.$uneLigne->datedebut.'<br>'.$uneLigne->datefin.'</th>';
+            endforeach 
+        ?>
+    </tr>
+
+    <?php
+        foreach ($categorie as $uneCategorie)
+        {
+            foreach($nombre as $unNombre)
+            {
+                if ($unNombre->LETTRECATEGORIE == $uneCategorie->LETTRECATEGORIE)
+                {
+                    echo '<tr>';
+                        echo '<th rowspan='.$unNombre->NOMBRE.'>';
+                        echo ''.$uneCategorie->LETTRECATEGORIE."</br>".$uneCategorie->LIBELLE.'';
+                        echo '</th>';
+                    foreach($type as $unType)
+                    {
+                        if ($unType->LETTRECATEGORIE == $uneCategorie->LETTRECATEGORIE)
+                        {
+                            if ($unType->NOTYPE != 1)
+                                echo '<tr>';
+                            
+                            echo '<td>'.$unType->LETTRECATEGORIE."".$unType->NOTYPE." - ".$unType->LIBELLE.'</td>';
+                            $numperiode = 1;
+                            foreach($tarif as $unTarif)
+                            {
+                                if ($unTarif->NOPERIODE == $numperiode && $unTarif->LETTRECATEGORIE == $unType->LETTRECATEGORIE && $unTarif->NOTYPE == $unType->NOTYPE )
+                                { 
+                                    echo '<td>'.$unTarif->TARIF.'</td>';
+                                    $numperiode++;
+                                }
+                            }      
+                            echo '</tr>'; 
+                        }                             
+                    }                 
+                }
+            }
+        }      
+    ?>
+</table>
